4df1ad5f70637f186cc95b0317da9771db733cd2
John Hamm <johndhamm1@gmail.com>
2016-07-11 11:21:25 -0500

    version 3 and 4 functional

--- xhr2.js
+++ xhr2.js
@@ -1,10 +1,40 @@
 var outputEl = document.getElementById("main");
-var firstList;
+var addView = document.getElementById("addSongSection");
+var songList; //hold array of all songs
+var songsAdded = false; //determine if 2nd set of songs have been loaded
+
+var viewSongsLink = document.getElementById("viewSongs");
+viewSongsLink.addEventListener("click", switchToSongsView);
+var addSongLink = document.getElementById("addSong");
+addSongLink.addEventListener("click", switchToAddSongView);
+var sideBarEl = document.getElementById("sidebar");
+
+function switchToSongsView() {
+	console.log("clicked view songs");
+	outputEl.classList.add("visible");
+	outputEl.classList.remove("hidden");
+	addView.classList.add("hidden");
+	addView.classList.remove("visible");
+	sideBarEl.classList.add("visible");
+	sideBarEl.classList.remove("hidden");
+}
+
+function switchToAddSongView () {
+	console.log("clicked add song");
+	outputEl.classList.add("hidden");
+	outputEl.classList.remove("visible");
+	addView.classList.add("visible");
+	addView.classList.remove("hidden");
+	sideBarEl.classList.add("hidden");
+	sideBarEl.classList.remove("visible");
+}
+
 
 function loadSongs () {
 	var data = JSON.parse(event.target.responseText);
-	firstList = data;
-	displaySongList(data);
+	songList = data;
+	console.log("songList", songList);
+	displaySongList(songList);
 }
 
 
@@ -12,11 +42,14 @@ function displaySongList (list) {
 	var listLength = list.songs.length;
 	outputEl.innerHTML = "";
 	for (var i = 0; i < listLength; i++) {
-		outputEl.innerHTML += `<section id="section${i}" class="song"><h2 class="songName">${list.songs[i].title}</h2><p class="artistName">${list.songs[i].artist}</p><p class="albumName">${list.songs[i].album}</p><button id="delBtn${i}" class="buttons">Delete Song</button></section>`;
+		outputEl.innerHTML += `<section id="section--${i}" class="song"><h2 class="songName">${list.songs[i].title}</h2><p class="artistName">${list.songs[i].artist}</p><p class="albumName">${list.songs[i].album}</p><button id="delBtn--${i}" class="buttons">Delete Song</button></section>`;
 	};
-	outputEl.innerHTML += `<div id="more"><button id="moreButton">More songs</button></div>`;
-	var moreButtonPress = document.getElementById("moreButton");
-	moreButtonPress.addEventListener("click", clickedMoreSongs);
+
+	if (songsAdded === false) { //if 2nd set of songs has not been added yet, add button for more songs
+		outputEl.innerHTML += `<div id="more"><button id="moreButton">More songs</button></div>`;
+		var moreButtonPress = document.getElementById("moreButton");
+		moreButtonPress.addEventListener("click", clickedMoreSongs);
+	}
 
 	var delButtonPress = document.getElementsByClassName("buttons");
 	for (var j = 0; j < listLength; j++) {
@@ -26,7 +59,12 @@ function displaySongList (list) {
 
 
 function deleteSong(clickedButton) {
-	clickedButton.currentTarget.parentNode.remove();
+	var clickedBtnID = event.target.id.split("--")[1]; //get ID # of clicked delete button
+	console.log("clicked button ID:", clickedBtnID);
+	songList.songs.splice(clickedBtnID, 1);
+	console.log("songList", songList);
+	// clickedButton.currentTarget.parentNode.remove();
+	displaySongList(songList);
 }
 
 
@@ -40,14 +78,15 @@ function clickedMoreSongs (clickMoreButton) {
 
 function appendList() {
 	var newData = JSON.parse(event.target.responseText);
-	// here's where need to create 1 object with array from the first and second json files
-	var newList = firstList;
-	for (var i = 0; i < newData.songs.length; i++) {
+
+	for (var i = 0; i < newData.songs.length; i++) { //add new songs from 2nd JSON to current song list array
 		var newSongObject = newData.songs[i]
-		newList.songs.push(newSongObject);
+		songList.songs.push(newSongObject);
 	}
-	displaySongList(newList);
+	songsAdded = true;
+	displaySongList(songList);
 }
+
 
 
 var myRequest = new XMLHttpRequest();
